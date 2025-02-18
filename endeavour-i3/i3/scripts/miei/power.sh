1b1e91d12a4e2261169eb176b981f7735be0ea56
Joni Rapo <jonirapo@gmail.com>
2025-02-18 08:07:20 +0200

    updated battery script

--- endeavour-i3/i3/scripts/miei/power.sh
+++ endeavour-i3/i3/scripts/miei/power.sh
@@ -4,6 +4,26 @@
 #echo "#a6e3a1"
 #
 
+if [[ "$BLOCK_BUTTON" -eq 1 ]]; then 
+    # src: i3blocks-contrib/batterybar
+    readarray -t output <<< $(acpi battery)
+    for line in "${output[@]}";
+    do
+		percentages+=($(echo "$line" | grep -o -m1 '[0-9]\{1,3\}%' | tr -d '%'))
+		statuses+=($(echo "$line" | egrep -o -m1 'Discharging|Charging|AC|Full|Unknown'))
+        remaining=$(echo "$line" | egrep -o -m1 '[0-9][0-9]:[0-9][0-9]')
+        if [[ -n $remaining ]]; then
+            remainings+=(" ($remaining)")
+        else 
+            remainings+=("")
+        fi
+    done
+    #echo -n "$(~/.config/i3/scripts/ppd-status)${remainings[i]}  "
+	echo -n "${percentages[$i]}%${remainings[i]} "
+elif [[ "$BLOCK_BUTTON" -eq 3 ]]; then 
+    ~/.config/i3/scripts/power-profiles
+fi
+
 #if [ $(cat /sys/class/power_supply/ACAD/online) == 1 ];then
 # Upper case is important in Charging, otherwise it matches Discharging
 if [[ $(acpi battery) =~ Charging ]]; then
