5d5b2633af40ab77de85a0e87e8d4e5aeb962aea
Joni Rapo <jonirapo@gmail.com>
2025-11-05 11:17:12 +0200

    update wordcount.sh

--- bin/wordcount.sh
+++ bin/wordcount.sh
@@ -5,6 +5,7 @@ LINES=$(cat "$1" | wc -l)
 if [[ -z "$START" ]]; then START=0; fi
 if [[ -z "$END" ]]; then END=$LINES; fi
 
-echo $LINES $START $END
-tail -n $(($LINES - $START)) "$1" | head -n $(($END - $START)) - | tr ' ' '\n' | wc -l
+echo "DEBUG: LINES=$LINES START=$START END=$END"
+echo -n "Words: "
+tail -n $(($LINES - $START)) "$1" | head -n $(($END - $START)) - | tr -d '\n' | tr ' ' '\n' | wc -l
 
